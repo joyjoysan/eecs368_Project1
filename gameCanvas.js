13db6096df8acccd7f03cbaa8b625536b2d02924
Victorina Joy Santos <santosednasian@icloud.com>
2020-03-30 20:52:30 -0500

    commit

--- gameCanvas.js
+++ gameCanvas.js
@@ -6,28 +6,54 @@ window.addEventListener('DOMContentLoaded', () => {
     function resizeCanvas(){
         canvas.width = window.innerWidth;
         
-        draw();
+        drawCanvas();
     }
 
-    function draw(){
+    function drawCanvas(){
         const canvas = document.getElementById('canvas');
         const context = canvas.getContext('2d');
 
-        //Grid
-        context.fillStyle = 'blue';
-        let numx = 350;
+        //Grid-Squares
+        let numx = 250;
         let numy = 95;
         for(let i = 1; i <= 7; i++){
-            numx = numx + 90
+            numx = numx + 90;
             numy = 95;
-            context.fillRect(numx,numy,75,75);           
+            fillSquares(numx, numy)
+            fillBorder(numx, numy);
+            fillCircle(numx, numy);
             for(let m = 1; m <= 5; m++){
                 numy = numy + 90
-                context.fillRect(numx,numy,80,80)
+                fillSquares(numx, numy);
+                fillBorder(numx, numy);
+                fillCircle(numx, numy);
             }
         }
 
-        //Slot Buttons
+
+        function fillSquares(numx, numy){
+            context.beginPath();
+            context.rect(numx,numy,75,75);   
+            context.fillStyle = "rgba(102, 178, 255, 1)";
+            context.fill();
+        }
+
+        function fillBorder(numx, numy){
+            context.lineWidth = 10;
+            context.strokeStyle = "rgba(31, 136, 255, 1)";
+            context.strokeRect(numx, numy, 80, 80);
+        }
+
+        function fillCircle(numx, numy){
+            context.beginPath();
+            context.arc(numx + 40, numy + 40, 30, 0, 2 * Math.PI, false);
+            context.fillStyle = "white";
+            context.fill();
+        }
     }
+
+    
+
+
 
 });
